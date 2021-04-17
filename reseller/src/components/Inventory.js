c7e98458ab36a888da9502461bcf12e7b800b829
Thomas Kahessay <Kahessaythomas@gmail.com>
2021-04-17 13:16:57 -0600

    some comments added

--- reseller/src/components/Inventory.js
+++ reseller/src/components/Inventory.js
@@ -21,25 +21,31 @@ import "./Inventory.scss";
 import axios from "axios";
 
 const Inventory = () => {
-  const [items, setItems] = useState([]);
-  const [showAddItem, setShowAddItem] = useState(false);
-  const [category, setCategory] = useState();
-  const [name, setName] = useState("");
-  const [model, setModel] = useState("");
-  const [brand, setBrand] = useState("");
-  const [size, setSize] = useState("");
-  const [year, setYear] = useState();
-  const [boughtPrice, setBoughtPrice] = useState();
-  const [askingPrice, setAskingPrice] = useState();
-  const [showAlert, setShowAlert] = useState(false);
+  const [items, setItems] = useState([]); // inventory items
+  const [showAddItem, setShowAddItem] = useState(false); // add item modal
+  const [category, setCategory] = useState(); // category in add item modal
+  const [name, setName] = useState(""); // name in add item modal
+  const [model, setModel] = useState(""); // model in add item modal
+  const [brand, setBrand] = useState(""); // brand in add item modal
+  const [size, setSize] = useState(""); // size in add item modal
+  const [year, setYear] = useState(); // year in add item modal
+  const [boughtPrice, setBoughtPrice] = useState(); // bought price in add item modal
+  const [askingPrice, setAskingPrice] = useState(); // asking price in add item modal
+  const [showAlert, setShowAlert] = useState(false); // alert for modal
 
   const url = window.location.href;
-  const id = url.split("/").pop();
+  const id = url.split("/").pop(); // getting the user id from the url
 
+  /**
+   * Retrieve items on page load
+   */
   useEffect(() => {
     retrieveItems();
   }, []);
 
+  /**
+   * Get request for items table for the specific seller
+   */
   const retrieveItems = () => {
     const sellerData = {
       id: id,
@@ -62,6 +68,9 @@ const Inventory = () => {
       .catch((e) => console.log(e));
   };
 
+  /**
+   * Post request for electronics table for the specific seller
+   */
   function addElectronic() {
     const itemData = {
       status: "AVAILABLE",
@@ -90,6 +99,9 @@ const Inventory = () => {
       .catch((e) => console.log(e));
   }
 
+  /**
+   * Post request for shoe table for the specific seller
+   */
   function addShoe() {
     const itemData = {
       status: "AVAILABLE",
@@ -119,6 +131,9 @@ const Inventory = () => {
       .catch((e) => console.log(e));
   }
 
+  /**
+   * Post request for card table
+   */
   function addCard() {
     const itemData = {
       status: "AVAILABLE",
@@ -148,10 +163,16 @@ const Inventory = () => {
       .catch((e) => console.log(e));
   }
 
+  /**
+   * Format asking price
+   */
   function formatAsk() {
     setAskingPrice(parseFloat(askingPrice).toFixed(2));
   }
 
+  /**
+   * Format bought price
+   */
   function formatBought() {
     setBoughtPrice(parseFloat(boughtPrice).toFixed(2));
   }
@@ -176,22 +197,22 @@ const Inventory = () => {
     >
       <IonGrid>
         <IonRow>
-          <IonCol className="header" size="0.5">
+          <IonCol className='header' size='0.5'>
             ID
           </IonCol>
-          <IonCol className="header" size="4">
+          <IonCol className='header' size='4'>
             Name
           </IonCol>
-          <IonCol className="header" size="1.5">
+          <IonCol className='header' size='1.5'>
             Bought Price
           </IonCol>
           <IonCol className='header' size='1.5'>
             Asking Price
           </IonCol>
-          <IonCol className="header" size="2">
+          <IonCol className='header' size='2'>
             Category
           </IonCol>
-          <IonCol className="header" size="2">
+          <IonCol className='header' size='2'>
             Status
           </IonCol>
         </IonRow>
@@ -211,9 +232,9 @@ const Inventory = () => {
           onClick={() => {
             setShowAddItem(true);
           }}
-          className="addBtn"
-          shape="round"
-          size="default"
+          className='addBtn'
+          shape='round'
+          size='default'
         >
           Add Item
         </IonButton>
@@ -221,95 +242,98 @@ const Inventory = () => {
       <IonContent>
         <IonModal
           isOpen={showAddItem}
-          cssClass="my-custom-class"
-          onDidDismiss={() => setShowAddItem(false)}
-          className="addModal"
+          cssClass='my-custom-class'
+          onDidDismiss={() => {
+            setShowAddItem(false);
+            clearData();
+          }}
+          className='addModal'
         >
           <h1>Add Item</h1>
           <IonList>
             <IonItem>
               <IonLabel>Category</IonLabel>
               <IonSelect
-                type="text"
+                type='text'
                 value={category}
-                placeholder="Select One"
+                placeholder='Select One'
                 onIonChange={(e) => setCategory(e.detail.value)}
               >
-                <IonSelectOption value="ELECTRONICS">
+                <IonSelectOption value='ELECTRONICS'>
                   Electronic
                 </IonSelectOption>
-                <IonSelectOption value="SHOE">Shoe</IonSelectOption>
-                <IonSelectOption value="CARD">Card</IonSelectOption>
+                <IonSelectOption value='SHOE'>Shoe</IonSelectOption>
+                <IonSelectOption value='CARD'>Card</IonSelectOption>
               </IonSelect>
             </IonItem>
             <IonItem>
               <IonInput
-                type="text"
+                type='text'
                 value={name}
-                placeholder="Name"
-                maxlength="255"
+                placeholder='Name'
+                maxlength='255'
                 onIonChange={(e) => setName(e.detail.value)}
               ></IonInput>
             </IonItem>
             <IonItem>
               <IonInput
-                type="text"
+                type='text'
                 value={model}
-                placeholder="Model"
-                maxlength="255"
+                placeholder='Model'
+                maxlength='255'
                 onIonChange={(e) => setModel(e.detail.value)}
               ></IonInput>
             </IonItem>
             <IonItem>
               <IonInput
-                type="text"
+                type='text'
                 value={brand}
-                placeholder="Brand"
-                maxlength="255"
+                placeholder='Brand'
+                maxlength='255'
                 onIonChange={(e) => setBrand(e.detail.value)}
               ></IonInput>
             </IonItem>
             <IonItem hidden={category !== "CARD"}>
               <IonInput
-                type="number"
+                type='number'
                 value={year}
-                placeholder="Year"
+                placeholder='Year'
                 onIonChange={(e) => setYear(parseInt(e.detail.value))}
               ></IonInput>
             </IonItem>
             <IonItem hidden={category !== "SHOE"}>
               <IonLabel>Size</IonLabel>
               <IonSelect
-                type="text"
+                type='text'
                 value={size}
-                placeholder="Select a Size"
+                placeholder='Select a Size'
                 onIonChange={(e) => setSize(e.detail.value)}
               >
-                <IonSelectOption value="4">4</IonSelectOption>
-                <IonSelectOption value="4.5">4.5</IonSelectOption>
-                <IonSelectOption value="5">5</IonSelectOption>
-                <IonSelectOption value="6">6</IonSelectOption>
-                <IonSelectOption value="6.5">6.5</IonSelectOption>
-                <IonSelectOption value="7">7</IonSelectOption>
-                <IonSelectOption value="7.5">7.5</IonSelectOption>
-                <IonSelectOption value="8">8</IonSelectOption>
-                <IonSelectOption value="8.5">8.5</IonSelectOption>
-                <IonSelectOption value="9">9</IonSelectOption>
-                <IonSelectOption value="9.5">9.5</IonSelectOption>
-                <IonSelectOption value="10">10</IonSelectOption>
-                <IonSelectOption value="10.5">10.5</IonSelectOption>
-                <IonSelectOption value="11">11</IonSelectOption>
-                <IonSelectOption value="11.5">11.5</IonSelectOption>
-                <IonSelectOption value="12">12</IonSelectOption>
-                <IonSelectOption value="12.5">12.5</IonSelectOption>
-                <IonSelectOption value="13">13</IonSelectOption>
+                <IonSelectOption value='4'>4</IonSelectOption>
+                <IonSelectOption value='4.5'>4.5</IonSelectOption>
+                <IonSelectOption value='5'>5</IonSelectOption>
+                <IonSelectOption value='6'>6</IonSelectOption>
+                <IonSelectOption value='6.5'>6.5</IonSelectOption>
+                <IonSelectOption value='7'>7</IonSelectOption>
+                <IonSelectOption value='7.5'>7.5</IonSelectOption>
+                <IonSelectOption value='8'>8</IonSelectOption>
+                <IonSelectOption value='8.5'>8.5</IonSelectOption>
+                <IonSelectOption value='9'>9</IonSelectOption>
+                <IonSelectOption value='9.5'>9.5</IonSelectOption>
+                <IonSelectOption value='10'>10</IonSelectOption>
+                <IonSelectOption value='10.5'>10.5</IonSelectOption>
+                <IonSelectOption value='11'>11</IonSelectOption>
+                <IonSelectOption value='11.5'>11.5</IonSelectOption>
+                <IonSelectOption value='12'>12</IonSelectOption>
+                <IonSelectOption value='12.5'>12.5</IonSelectOption>
+                <IonSelectOption value='13'>13</IonSelectOption>
               </IonSelect>
             </IonItem>
             <IonItem>
               <IonInput
-                type="number"
+                type='number'
                 value={boughtPrice}
-                placeholder="Bought Price"
+                placeholder='Bought Price'
                 onIonChange={(e) => {
                   setBoughtPrice(e.detail.value);
                 }}
@@ -320,9 +344,9 @@ const Inventory = () => {
             </IonItem>
             <IonItem>
               <IonInput
-                type="number"
+                type='number'
                 value={askingPrice}
-                placeholder="Asking Price"
+                placeholder='Asking Price'
                 onIonChange={(e) => setAskingPrice(e.detail.value)}
                 onPointerLeave={() => {
                   formatAsk();
@@ -332,19 +356,22 @@ const Inventory = () => {
           </IonList>
           <IonGrid>
             <IonRow>
-              <IonCol size="6">
+              <IonCol size='6'>
                 <IonButton
                   expand='full'
-                  onClick={() => setShowAddItem(false)}
+                  onClick={() => {
+                    setShowAddItem(false);
+                    clearData();
+                  }}
                   className='cancelBtn'
                   shape='round'
                 >
                   Cancel
                 </IonButton>
               </IonCol>
-              <IonCol size="6">
+              <IonCol size='6'>
                 <IonButton
-                  expand="full"
+                  expand='full'
                   onClick={() => {
                     if (
                       !category ||
@@ -376,7 +403,7 @@ const Inventory = () => {
                       addCard();
                     }
                   }}
-                  shape="round"
+                  shape='round'
                 >
                   Add
                 </IonButton>
@@ -386,7 +413,7 @@ const Inventory = () => {
           <IonAlert
             isOpen={showAlert}
             onDidDismiss={() => setShowAlert(false)}
-            message="Please Enter All Fields"
+            message='Please Enter All Fields'
             buttons={[
               {
                 text: "OK",
